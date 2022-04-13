a0d98f840c6a414da6e53922c7e2edc7cfe11430
ilyaFD <ilya.frontd@gmail.com>
2022-04-13 21:04:30 +0200

    init

---
+++ src/hooks/useFetch.hook.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+
+export default function useFetch <T>(url: string) {
+	const [response, setResponse] = React.useState<T[] | null>(null);
+    const [isError, setIsError] = React.useState<boolean>(false);
+    const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+    React.useEffect(() => {
+        function fetchCountries(): void {
+            axios
+                .get(url)
+                .then(resp => {
+                    setIsLoading(false);
+                    setResponse(resp.data);
+                })
+                .catch(err => {
+                    setIsLoading(false);
+                    setIsError(true);
+                });
+        }
+
+        fetchCountries();
+    }, [])
+
+    return [ response, isError, isLoading ]  as const;
+}
---
+++ src/stores/Store.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { makeAutoObservable } from "mobx";
+
+export interface ICountry {
+  name: string;
+  continent: string;
+}
+
+export interface IStore {
+  activeRegion: string;
+  countries: ICountry[]; 
+  activeCountries: string[]; 
+}
+
+export class Store implements IStore {
+  activeRegion = '';
+  countries = [] as ICountry[];
+  activeCountries = [] as string[];
+
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setActiveRegion(activeRegion: string) {
+    this.activeRegion = activeRegion;
+  }
+
+  setCountries(countries: ICountry[]) {
+    this.countries = countries;
+  }
+
+  addActiveCountry(country: string) {
+    // this.activeCountries = [...this.activeCountries, country]
+    this.activeCountries.push(country)
+  }
+
+  deleteActiveCountry(country: string) {
+    this.activeCountries = this.activeCountries.filter((item: string) => item !== country)
+  }
+}
+
+/* Store provider */
+const StoreContext = React.createContext({} as IStore); 
+
+type StoreComponent = React.FC<{
+  store: IStore;
+  children: React.ReactNode;
+}>;
+
+export const StoreProvider: StoreComponent = ({ children, store }) => {
+  return (
+    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+  );
+};
+
+/* Hook to use store in any functional component */
+export const useStore = (): IStore => React.useContext(StoreContext);
